eb65d60eabb39938a97662c80643ef8444ac01b0
Akshay achar <acharyaakshay445@gmail.com>
2025-07-06 13:08:03 +0000

    Updated updatek8sManifests.sh

--- scripts/updatek8sManifests.sh
+++ scripts/updatek8sManifests.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-APP_NAME=$1
+SERVICE=$1
 IMAGE=$2
 
-echo "Updating image in $APP_NAME manifest..."
-sed -i "s|image: .*|image: $IMAGE|g" k8s/${APP_NAME}-deployment.yaml
+# Example sed (adjust as needed)
+sed -i "s|image:.*|image: $IMAGE|g" k8s-manifests/$SERVICE-deployment.yaml
+
